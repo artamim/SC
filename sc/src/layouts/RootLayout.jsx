4bbecd0b1560f8f9ff6966783b950973e82bf9e9
Arifur Rahman Tamim <artamim22@gmail.com>
2024-11-18 18:30:39 +0600

    Page color and design

---
+++ sc/src/layouts/RootLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink, Outlet} from 'react-router-dom';
+import '../styles/RootLayout.css';
+
+export default function RootLayout() {
+    return (
+        <header>
+            <nav>
+                <div></div>
+                <ul className="right-menu">
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="about">About</NavLink></li>
+                </ul>
+            </nav>
+            <main>
+                <Outlet />
+            </main>
+        </header>
+    );
+}
--- sc/src/main.jsx
+++ sc/src/main.jsx
@@ -1,21 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import './index.css'
+
+//Pages
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
-import Header from './pages/Header.jsx'
+
+//Layouts
+import RootLayout from './layouts/RootLayout.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </>
+  )
+)
+
+function Main(){
+  return(
+    <div className="main-body">
+      <RouterProvider router={router} />
+    </div>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Header />
-      <div className="main-body">
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+      <Main />
   </StrictMode>,
 )
