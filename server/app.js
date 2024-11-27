02f9d18dafdb35551a2218c1735cdb22323c3591
Arifur Rahman Tamim <artamim22@gmail.com>
2024-11-27 18:07:59 +0600

    made some changes to app.js

--- server/app.js
+++ server/app.js
@@ -9,19 +9,6 @@ app.use(express.json());
 
 app.use("/users", usersRoutes);
 
-app.get("/", async (req, res) => {
-    try {
-      const result = await pool.query("SELECT * FROM users");
-      const formattedResponse = result.rows.map(
-        (user) => `User Code ${user.xuser} belongs to ${user.xusername}`
-      );
-      res.json(result.rows); // Return all users
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
-  });
-
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
