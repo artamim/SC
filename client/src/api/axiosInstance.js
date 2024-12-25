7c80592dde9b4ed86560f91d8d756adc5cc2aae5
Arifur Rahman Tamim <artamim22@gmail.com>
2024-12-25 14:30:31 +0600

    Axios check

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -4,5 +4,6 @@ const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_PROD_BASE_URL || "http://localhost:5000",
   withCredentials: true,
 });
+console.log("Base URL:", import.meta.env.VITE_PROD_BASE_URL);
 
 export default axiosInstance;
